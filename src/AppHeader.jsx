8181341b8c38bd90cc8b95903daebb01c08512b3
Rubacova Polina <rubacova.polina@yandex.ru>
2023-02-25 16:45:13 +0300

    Сделана шапка

--- src/AppHeader.jsx
+++ src/AppHeader.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import {Logo, } from '@ya.praktikum/react-developer-burger-ui-components';
+import {Logo, Box, BurgerIcon, ListIcon, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 
 export default function AppHeader () {
   return (
-      <header className="App-header">
+      <header className="header p-4">
         <nav className='menu'>
-        <div className="menu__item"></div>
-        <div className="menu__item"></div>
+          <div className="menu__item p-5">
+            <BurgerIcon type="primary" />
+            <p className="text text_type_main-default">
+            Конструктор</p>
+          </div>
+          <div className="menu__item p-5">
+            <ListIcon type="primary" />
+            <p className="text text_type_main-default">
+            Лента заказов</p>
+          </div>
         </nav>
         <Logo />
-        <div className="menu__item"></div>
+        <div className="menu__item p-5">
+          <ProfileIcon type="primary" />
+          <p className="text text_type_main-default">
+          Личный кабинет</p>
+        </div>
       </header>
   );
 }
